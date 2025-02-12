563322bf32eb516ab88a5ec975f2263dd569d416
fxtih61 <fatih.tolip@web.de>
2025-02-12 21:20:31 +0100

    feat: adds BootstrapFX configuration

--- src/main/java/com/openjfx/App.java
+++ src/main/java/com/openjfx/App.java
@@ -1,8 +1,14 @@
 package com.openjfx;
 
+import org.kordamp.bootstrapfx.BootstrapFX;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,8 +23,23 @@ public class App extends Application {
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version");
     Label label = new Label("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion);
-    Scene scene = new Scene(new StackPane(label), 640, 480);
+    // Scene scene = new Scene(new StackPane(label), 640, 480);
+
+    Panel panel = new Panel("This is the title");
+    panel.getStyleClass().add("panel-primary");
+    BorderPane content = new BorderPane();
+    content.setPadding(new Insets(20));
+    Button button = new Button("Hello BootstrapFX");
+    button.getStyleClass().setAll("btn", "btn-danger");
+    content.setCenter(button);
+    panel.setBody(content);
+
+    Scene scene = new Scene(panel);
+    scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+
+    stage.setTitle("Hello BootstrapFX");
     stage.setScene(scene);
+    stage.sizeToScene();
     stage.show();
   }
 
